8ddccdf1f07692fb5b6c325f98ce6d4df34b9ce9
Andrew Ramirez <alramir3@uci.edu>
2021-08-31 08:42:05 -0700

    html fixes and starting JS

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-console.log("Connection made")
+
+
+class Data {
+    constructor(totalBill, numGuests, servQual) {
+        this.totalBill = totalBill,
+        this.numGuests = numGuests,
+        this.servQual = servQual
+    }
+}
+
+let tipData = Data(document.getElementById("totalBill"), document.getElementById("numGuests"), document.getElementById("ServQual"))
+
+console.log(tipData)
